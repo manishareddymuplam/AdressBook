6265525c5087d5b66f217a6b92f2156a845bbc14
manishareddymuplam <manishamuplam@gmail.com>
2022-07-10 11:33:36 +0530

    added:EditContactWithFirstName

--- src/com/bridgelabz/adressbook/AdressBookMain.java
+++ src/com/bridgelabz/adressbook/AdressBookMain.java
@@ -5,5 +5,18 @@ public class AdressBookMain {
 	        System.out.println("Welcome to Address Book System");
 	        AddressBook addressBook = new AddressBook();
 	        addressBook.addContact();
+	        addressBook.addContact();
+	        //UC2 add contacts
+	        for (Contacts contacts : addressBook.contactsArrayList) {
+	            System.out.println("The" + contacts);
+	        }
+	        //UC3 edit contacts
+	        addressBook.editContacts();
+	        System.out.println("After editing: ");
+	        System.out.println("*********************");
+	        for (Contacts contacts:addressBook.contactsArrayList) {
+	            System.out.println("the"+contacts);
+	        }
+	        System.out.println("*********************");
 	    }
 	}
