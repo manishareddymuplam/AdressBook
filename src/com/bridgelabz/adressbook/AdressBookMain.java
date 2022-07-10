aa279231b05ff7bc3c454867bf1fe9772b20a34a
manishareddymuplam <manishamuplam@gmail.com>
2022-07-10 11:54:42 +0530

    added:MultiplePersons

--- src/com/bridgelabz/adressbook/AdressBookMain.java
+++ src/com/bridgelabz/adressbook/AdressBookMain.java
@@ -1,9 +1,28 @@
 package com.bridgelabz.adressbook;
 
 public class AdressBookMain {
-	 public static void main(String[] args) {
-	        System.out.println("Welcome to Address Book System");
-	        AddressBook addressBook = new AddressBook();
-	        addressBook.addContact();
-	    }
-	}
+	public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+        addressBook.addContact();
+        //UC2 add contacts
+        for (Contacts contacts : addressBook.contactsArrayList) {
+            System.out.println("The" + contacts);
+        }
+        //UC3 edit contacts
+        addressBook.editContacts();
+        System.out.println("After editing: ");
+        System.out.println("*********************");
+        for (Contacts contacts:addressBook.contactsArrayList) {
+            System.out.println("the"+contacts);
+        }
+        System.out.println("*********************");
+//
+        //UC4 delete users
+        addressBook.deleteContacts();
+        for (Contacts contacts:addressBook.contactsArrayList) {
+            System.out.println("the" + contacts);
+        }
+    }
+}
