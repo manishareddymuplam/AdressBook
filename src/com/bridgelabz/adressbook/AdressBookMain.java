997f366df305e7c8c8bf595ec2d34d134d18b887
manishareddymuplam <manishamuplam@gmail.com>
2022-07-10 11:08:31 +0530

    added:AdressBook

---
+++ src/com/bridgelabz/adressbook/AdressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.adressbook;
+
+public class AdressBookMain {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book System");
+	        AddressBook addressBook = new AddressBook();
+	        addressBook.addContact();
+	    }
+	}
